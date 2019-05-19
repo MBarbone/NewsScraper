7d42c7e8b82f14d5bb331adf5c533159b51a0887
Mike <michael.barbone@gmail.com>
2019-05-19 00:16:58 -0400

    Bring in Morgan for logging requests. Configure Middleware

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
+const logger = require("morgan");
+// mongoose connection
 const db = require("./db/connection");
-
+const axios = require("axios");
+const cheerio = require("cheerio");
+const logger = require("morgan");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+// middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
